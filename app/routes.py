95296578d1f637b5faa7a212719a8654e148a481
Exoutia <jhav365@gmail.com>
2022-12-19 15:17:09 +0530

    modifiying the view of index using django using templates

--- app/routes.py
+++ app/routes.py
@@ -1,16 +1,8 @@
+from flask import render_template
 from app import app
 
 @app.route('/') # Decorator that tells Flask what URL should trigger our function
 @app.route('/index') # Decorator that tells Flask what URL should trigger our function
 def index():
     user = {'username': 'Bibek'}
-    return '''
-    <html>
-    <head>
-        <title>Home Page - Microblog</title>
-    </head>
-    <body>
-        <h1>Hello, ''' + user['username'] + '''!</h1>
-    </body>
-    </html>
-    '''
+    return render_template('index.html', title='Home', user=user)
