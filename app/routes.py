831d82d7b41d8b0abbf7ca66b49e9cfd8a6bdd72
Exoutia <jhav365@gmail.com>
2022-12-19 20:15:59 +0530

    Adding some posts

--- app/routes.py
+++ app/routes.py
@@ -6,4 +6,18 @@ from app import app
 def index():
     # This is here we are making a dummy user for our app this is just tes should be deleted after some points
     user = {'username': 'Bibek'}
-    return render_template('index.html', title='Home', user=user)
+    posts = [
+        {
+            'author': {'username': 'Jhon'},
+            'body': 'Beautiful day in India!'
+        },
+        {
+            'author': {'username': 'Alice'},
+            'body': 'Boy we are demons who control the fate'
+        },
+        {
+            'author': {'username': 'Gummy_Bear_of_legends'},
+            'body': 'You sucker of sugar I am the one who will control the universe'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
