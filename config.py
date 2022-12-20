6333c295454c0bd100bccfc8528cc53c8a60babd
Exoutia <jhav365@gmail.com>
2022-12-20 22:09:57 +0530

    confging varibale of sqlalchemy setting the track_modifictaions to false

--- config.py
+++ config.py
@@ -1,5 +1,8 @@
 '''Thid is the file where config variables are stored'''
 import os
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False # This is just here to suppress a warning from SQLAlchemy as it will soon be removed
