337569757831d548173554ea2e0d267328ac04ff
Exoutia <jhav365@gmail.com>
2022-12-20 19:03:55 +0530

    adding the route to logins

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,8 @@
 from flask import Flask
+from config import Config
+
 
 app = Flask(__name__) # Create an instance of the Flask class using '__name__' as the name of the application
+app.config.from_object(Config) # Load the configuration from the Config class
 
 from app import routes # Import the routes module from app
