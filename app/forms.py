b82bee5e5118ee7d2f6db0ecdea3ab8232e8fe2f
Exoutia <jhav365@gmail.com>
2022-12-20 19:28:37 +0530

    adding comments

--- app/forms.py
+++ app/forms.py
@@ -2,6 +2,9 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, TextAreaField, BooleanField, SubmitField
 from wtform.validators import DataRequired
 
+# Most Flask extensions use a flask_<name> naming convention
+# for their top-level import symbol. In this case,
+# Flask-WTF has all its symbols under flask_wtf.
 class LoginForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
     password = StringField('Password', validators=[DataRequired()])
