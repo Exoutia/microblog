0b268d73f55395f93a296e4352469d45bc979e1d
Exoutia <jhav365@gmail.com>
2022-12-20 22:15:05 +0530

    initiatin the flask-sqlalchemy and flask-migrate

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,6 @@
 from flask import Flask
 from config import Config
-from falsk_sqlalchemy import SQLAlchemy
+from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
 
@@ -9,4 +9,4 @@ app.config.from_object(Config) # Load the configuration from the Config class
 db = SQLAlchemy(app) # Create an instance of the SQLAlchemy class using the app instance
 migrate = Migrate(app, db) # Create an instance of the Migrate class using the app and db instances
 
-from app import routes, models 
+from app import routes, models
