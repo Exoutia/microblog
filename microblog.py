bfb40cefe1413872c6a4c35a7aabea2627bec871
Exoutia <jhav365@gmail.com>
2022-12-21 09:43:21 +0530

    adding shell context processor

--- microblog.py
+++ microblog.py
@@ -1,2 +1,7 @@
 '''This is where main app is running and we are importing all the modules and packages that we need to run the app'''
-from app import app
+from app import app, db
+from app.models import User, Post
+
+@app.shell_context_processor
+def make_shell_context():
+    return {'db': db, 'User': User, 'Posr': Post}
