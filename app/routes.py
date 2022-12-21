958bbdd7078e7833e9a9fffc0610b58dfa85478b
Exoutia <jhav365@gmail.com>
2022-12-21 14:22:16 +0530

    applying the logic to request user to login and then redirect them to next page

--- app/routes.py
+++ app/routes.py
@@ -1,12 +1,14 @@
-from flask import render_template, flash, redirect, url_for
+from flask import render_template, flash, redirect, url_for, request
 from app import app
 from app.forms import LoginForm
-from flask_login import current_user, login_user, logout_user
+from flask_login import current_user, login_user, logout_user, login_required
 from app.models import User
+from werkzeug.urls import url_parse
 
 
 @app.route('/') # Decorator that tells Flask what URL should trigger our function
 @app.route('/index') # Decorator that tells Flask what URL should trigger our function
+@login_required
 def index():
     # This is here we are making a dummy user for our app this is just tes should be deleted after some points
     user = {'username': 'Bibek'}
@@ -48,7 +50,14 @@ def login():
 
         # If the user is found and the password is correct then we will login the user
         login_user(user, remember=form.remember_me.data)
-        return redirect(url_for('index'))
+
+        # this is used toredirect to the page the user is trying
+        # to get in when it gor here to first login and then go there
+
+        next_page = request.args.get('next')
+        if not next_page or url_parse(next_page).netloc != '':
+            next_page = url_for('index')
+        return redirect(url_for(next_page))
 
 
     return render_template('login.html', title='Sign In', form=form)
