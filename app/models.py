4a977f8a2185705bba6769cf40a324961d3d653b
Exoutia <jhav365@gmail.com>
2022-12-21 12:11:59 +0530

    connecting the flask login to database to keep track of the logged in user.

--- app/models.py
+++ app/models.py
@@ -1,8 +1,9 @@
 from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
 from datetime import datetime
-from app import db
+from app import db, login
 
-class User(db.Model):
+class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
@@ -17,6 +18,7 @@ class User(db.Model):
 
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
+
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     body = db.Column(db.String(140))
@@ -25,3 +27,13 @@ class Post(db.Model):
 
     def __repr__(self):
         return '<Post {}>'.format(self.body)
+
+
+
+# as flask login knows nothing about database, it needs the
+# application's help in loading a user for that reason
+# the extension expects that the application will configure
+# a user loader function, that can be called to load a user given the ID
+@login.user_loader
+def load_user(id):
+    return User.query.get(int(id))
