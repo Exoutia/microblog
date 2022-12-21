13c46977a9331834b49d66909242df669f69fdb0
Exoutia <jhav365@gmail.com>
2022-12-21 14:09:07 +0530

    adding the logic to login the user and saving the data to the database and also keep them logged in them. This is done by flask_login

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,8 @@
 from flask import render_template, flash, redirect, url_for
 from app import app
 from app.forms import LoginForm
+from flask_login import current_user, login_user, logout_user
+from app.models import User
 
 
 @app.route('/') # Decorator that tells Flask what URL should trigger our function
@@ -27,8 +29,33 @@ def index():
 # We are now adding the routes for login
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    form = LoginForm()
-    if form.validate_on_submit():
-        flash('login requested for user {}, remember_me={}'.format(form.username.data, form.remember_me.data))
+    # Chehck if the user is already logged in or not
+    if current_user.is_authenticated():
+        # return them to the index if user is already logged in
         return redirect(url_for('index'))
+
+    form = LoginForm()
+
+    # If the form is validated then we will check the username and password
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+
+        # if the user is not found or the password is wrong then we will flash the message
+        if user is None or not user.check_password(form.password.data):
+
+            flash('Invalid username or password')
+            return redirect(url_for('login'))
+
+        # If the user is found and the password is correct then we will login the user
+        login_user(user, remember=form.remember_me.data)
+        return redirect(url_for('index'))
+
+
     return render_template('login.html', title='Sign In', form=form)
+
+
+# this is function to logout the user
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
