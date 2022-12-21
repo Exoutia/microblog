72d24bc506df86065c2a1ee9c9da74c97f49eb4c
Exoutia <jhav365@gmail.com>
2022-12-21 16:20:43 +0530

    adding the register form logic

--- app/forms.py
+++ app/forms.py
@@ -1,12 +1,39 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms import StringField, SubmitField, PasswordField, BooleanField
+from wtforms.validators import DataRequired, ValidationError, Email, EqualTo
+from app.models import User
+
 
 # Most Flask extensions use a flask_<name> naming convention
 # for their top-level import symbol. In this case,
 # Flask-WTF has all its symbols under flask_wtf.
 class LoginForm(FlaskForm):
+    """This is the form we login with and it has field of username
+    functions are self explantory.
+
+    Args:
+        FlaskForm (class of flasl_login): use to make the form for flask website
+    """
     username = StringField('Username', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
     remember_me = BooleanField('Remember Me')
     submit = SubmitField('Sign In')
+
+class RegistrationForm(FlaskForm):
+    """this is the form for registration purpose."""
+    username = StringField('Username', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    passowrd2 = PasswordField('Repeat Password', validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Register')
+
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user is not None:
+            raise ValidationError('Please use a different username.')
+
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user is not None:
+            raise ValidationError('Please use a different email address.')
+        
