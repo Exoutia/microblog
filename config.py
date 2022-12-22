edc074349264878d573fb466da8d9290c1e654b7
Exoutia <jhav365@gmail.com>
2022-12-22 17:45:02 +0530

    fixing pylint issue

--- config.py
+++ config.py
@@ -2,7 +2,15 @@
 import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
-class Config(object):
+class Config(object): 
+    """This is the configuration class for the app which is used
+    for the different setting or decision we need to take for flask.
+    I am using it to provide different things to app as objects.
+    """
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'app.db')
-    SQLALCHEMY_TRACK_MODIFICATIONS = False # This is just here to suppress a warning from SQLAlchemy as it will soon be removed
+    SQLALCHEMY_DATABASE_URI = (os.environ.get('DATABASE_URL') or
+                                'sqlite:///' + os.path.join(basedir, 'app.db'))
+
+    # This is just here to suppress a warning from
+    # SQLAlchemy as it will soon be removed
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
