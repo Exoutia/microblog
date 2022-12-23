6c329e2a03704eaddba123fa78b37ee031d5c887
Exoutia <jhav365@gmail.com>
2022-12-23 10:00:31 +0530

    adding the new row 'about_me', 'last_seen' in table user

--- app/models.py
+++ app/models.py
@@ -11,6 +11,8 @@ class User(db.Model, UserMixin):
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
     posts = db.relationship('Post', backref='author', lazy='dynamic')
+    about_me = db.Column(db.String(150))
+    last_seen = db.Column(db.DateTime, default=datetime.utcnow)
 
     def __repr__(self):
         return f'<User {self.username}>'
@@ -24,7 +26,7 @@ class User(db.Model, UserMixin):
     def avatar(self, size):
         digest = md5(self.email.lower().encode('utf-8')).hexdigest() # nosec
         return f'https://www.gravatar.com/avatar/{digest}?d=identicon&s={size}'
-        
+
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     body = db.Column(db.String(140))
