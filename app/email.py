7a09f6a00bb0b1f22f0925443edbce024bc495cc
Exoutia <jhav365@gmail.com>
2022-12-30 08:28:33 +0530

    adding the sync process for the email to send in background while it will work for other process

--- app/email.py
+++ app/email.py
@@ -1,12 +1,25 @@
 from flask_mail import Message
 from app import mail, app
 from flask import render_template
+from threading import Thread # this is use for multipurpose
+
+# this is use for the async mail sending
+# as we know starting a background thread for email
+# being sent is much less resource intensive than starting
+# a brand new process, so I'm going to go with that approach
+def send_async_email(app, msg):
+    with app.app_context():
+        mail.send(msg)
 
 def send_email(subject, sender, recipients, text_body, htnl_body):
     msg = Message(subject, sender=sender, recipients=recipients)
     msg.body = text_body
     msg.html = htnl_body
-    mail.send(msg)
+    # he send_async_email function now runs in a background thread,
+    # invoked via the Thread class in the last line of send_email().
+    # With this change, the sending of the email will run in the thread,
+    # and when the process completes the thread will end and clean itself up.
+    Thread(target=send_async_email, args=(app, msg)).start() # this will free send email function to use another place while email is sending.
 
 
 
@@ -18,3 +31,4 @@ def send_password_reset_email(user):
         text_body=('email/reset_password.txt', user=user, token=token),
         html_body=render_template('email/reset_password.html', user=user, token=token)
     )
+
